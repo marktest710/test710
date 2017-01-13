99b96f057ae79675c47e1fc2e8a25fbd27d347c2
mark <mark.ipred@gmail.com>
2017-01-13 00:19:55 +0000

    added again

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -6,12 +6,12 @@
 
 
 package 'apache2' do
-	package_name 'httpd'
+	package_name 'apache2'
 	action :install
 end
 
 service 'apache2' do
-	service_name 'httpd'
+	service_name 'apache2'
 	action [:start, :enable]
 end
 
