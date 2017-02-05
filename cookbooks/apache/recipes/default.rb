b61000926886d269688765acdd01688ca4555cb5
mark <mark.ipred@gmail.com>
2017-02-05 20:24:03 +0000

    updates

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -1,9 +1,13 @@
-#
 # Cookbook:: apache
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
+if node['platform_family'] == "debian"
+	package = "apache2"
+elseif node ['platform_family'] == "rhel"
+	package = "httpd"
+end
 
 package 'apache2' do
 	package_name 'apache2'
@@ -14,4 +18,6 @@ service 'apache2' do
 	service_name 'apache2'
 	action [:start, :enable]
 end
+
+
 
